182fdee558effed70be30e90c5b56f2b44daa66a
Yuchi Deng <dengyuchi@gmail.com>
2016-02-01 14:41:01 -0800

    2/1 First version with easy functions

--- DailyKeyCount/AppDelegate.swift
+++ DailyKeyCount/AppDelegate.swift
@@ -7,21 +7,46 @@
 //
 
 import Cocoa
+import AppKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        if let menuBarButton = statusItem.button {
+            menuBarButton.image = NSImage(named: "menu-bar-icon")
+            menuBarButton.action = Selector("togglePopover:")
+        }
+        popover.contentViewController = MainViewController(nibName: "MainViewController", bundle: nil)
+        togglePopover(nil)
+        togglePopover(nil)
+    }
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MaxY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
---
+++ DailyKeyCount/MainViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MainViewController.swift
+//  DailyKeyCount
+//
+//  Created by DengYuchi on 2/1/16.
+//  Copyright © 2016 LateRain. All rights reserved.
+//
+
+import Cocoa
+import CoreText
+
+class MainViewController: NSViewController {
+    
+    var isQuitButtonPressedOnce: Bool = false
+    
+    @IBOutlet weak var keyCountLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        var normalKeyCount: Int64 = 0;
+        var specialKeyCount: Int64 = 0;
+        
+        keyCountLabel.font = NSFont.monospacedDigitSystemFontOfSize(32, weight: NSFontWeightThin)
+        
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        
+        NSEvent.addGlobalMonitorForEventsMatchingMask(NSEventMask.KeyDownMask) { (event: NSEvent) -> Void in
+            self.keyCountLabel.stringValue = numberFormatter.stringFromNumber(NSNumber(longLong: ++normalKeyCount + specialKeyCount / 2))!
+        }
+        NSEvent.addLocalMonitorForEventsMatchingMask(NSEventMask.KeyDownMask) { (evet: NSEvent) -> NSEvent? in
+            self.keyCountLabel.stringValue = numberFormatter.stringFromNumber(NSNumber(longLong: ++normalKeyCount + specialKeyCount / 2))!
+            return nil
+        }
+        
+        NSEvent.addGlobalMonitorForEventsMatchingMask(NSEventMask.FlagsChangedMask) { (event: NSEvent) -> Void in
+            self.keyCountLabel.stringValue = numberFormatter.stringFromNumber(NSNumber(longLong: normalKeyCount + (++specialKeyCount + 1) / 2))!
+        }
+        NSEvent.addLocalMonitorForEventsMatchingMask(NSEventMask.FlagsChangedMask) { (evet: NSEvent) -> NSEvent? in
+            self.keyCountLabel.stringValue = numberFormatter.stringFromNumber(NSNumber(longLong: normalKeyCount + (++specialKeyCount + 1) / 2))!
+            return nil
+        }
+    }
+    
+    @IBAction func quitButtonPressed(sender: AnyObject) {
+        if !self.isQuitButtonPressedOnce {
+            self.isQuitButtonPressedOnce = true
+        } else {
+            NSApplication.sharedApplication().terminate(sender)
+        }
+    }
+}
