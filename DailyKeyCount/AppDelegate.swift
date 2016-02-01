6fca3c3aed630fa7c009160a9b2337467f4c633f
Yuchi Deng <dengyuchi@gmail.com>
2016-02-01 13:01:56 -0800

    Initial Commit

---
+++ DailyKeyCount/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DailyKeyCount
+//
+//  Created by DengYuchi on 2/1/16.
+//  Copyright © 2016 LateRain. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
